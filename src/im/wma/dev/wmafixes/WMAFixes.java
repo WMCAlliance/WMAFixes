b17a94368cdac842d5d78ee8775bb17a41091e81
nathanblaney <nathanblaney98@gmail.com>
2013-10-13 19:39:14 +1100

    Added: Water flow

--- src/im/wma/dev/wmafixes/WMAFixes.java
+++ src/im/wma/dev/wmafixes/WMAFixes.java
@@ -1,5 +1,27 @@
 package im.wma.dev.wmafixes;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class WMAFixes {
-
+public class WMAFixes extends JavaPlugin implements Listener {
+	   
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("Blah 2");
+    }
+    
+	public void onDisable(){
+		getLogger().info("Blah 2");
+	}
+   
+    @EventHandler
+    public void onBlockFromTo(BlockFromToEvent event) {
+      int id = event.getBlock().getTypeId();
+      if(id == 8 || id == 9) {
+    	  getLogger().info("user placed water!");
+        event.setCancelled(true);
+      }
+    }
 }
